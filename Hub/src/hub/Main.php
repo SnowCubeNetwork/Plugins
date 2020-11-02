2c8f8eaf693305832b66a00efb309f45c0909b08
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-01 21:52:22 -0800

    Update Main.php

--- Hub/src/hub/Main.php
+++ Hub/src/hub/Main.php
@@ -3,11 +3,22 @@
 namespace hub;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase {
 
-	public function onEnable() {
+	public $config;
 
+	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		if (!file_exists($this->getDataFolder() . "config.yml")) {
+			$this->saveResource("config.yml");
+		}
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+			"title" => "SnowCube Network",
+			"server-ip" => "play.snowcube.ml"
+		]);
 	}
 
 }
